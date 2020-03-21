f8805f4eecd121ddca2a18ef16f821fbc3f3517d
Dominik Kwaczyński <dominik.kwaczynski@frogzee.com>
2020-03-22 00:10:08 +0100

    add check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2 === 0) {
+    let currentBrackets, trimString, bracketsComplete;
+
+    for (let i = 0, breakMe = true; i < bracketsConfig.length && breakMe; i++) {
+      currentBrackets = bracketsConfig[i].join('');
+      trimString = str.replace(currentBrackets, '');
+      if(trimString !== str) {
+        str = trimString;
+        str.length !== 0 ? i = -1 : breakMe = false;
+      }
+    }
+
+    str.length > 1 ? bracketsComplete = false : bracketsComplete = true;
+    return bracketsComplete;
+  } else return false;
 }
